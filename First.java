5fece917fd61ad7c3a1f404c1d24399c4ddcf475
Jason Morgan <shaddix.morgan@gmail.com>
2026-01-26 12:08:19 -0600

    Out first commit, assignment 1

--- First.java
+++ First.java
@@ -1,6 +1,13 @@
+//Jason Morgan
+//1-26-2026
+//Computer Science II Module 1 Assignment 1
+
 public class First {
     public static void main(String[] args)
-    { 
-        System.out.println("Tests");
+    {
+        System.out.println("Hola mundo");
+        System.out.println("Job title: Information Security Analysts");
+        System.out.println("Why: Because security plays a vital role and the job outlook is projected to keep growing over the years. ");
+        System.out.println("Median salary: $124,910");
     }
 }
