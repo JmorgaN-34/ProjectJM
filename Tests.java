d75864c5574f8a272b022cfdd7e9b487fac97f54
JmorgaN-34 <shaddix.morgan@gmail.com>
2026-02-02 11:44:21 -0600

    Test.java program calculates tests average

---
+++ Tests.java
@@ -0,0 +1,23 @@
+// Jason Morgan
+// 2-2-2026
+// Computer Science II Module 2 Assignment 2
+
+import java.text.DecimalFormat;
+
+public class Tests {
+    public static void main(String[] args) {
+
+        double testScore1 = 88.2;
+        double testScore2 = 78.9;
+        double testScore3 = 97.6;
+
+        double average = (testScore1 + testScore2 + testScore3) / 3;
+
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        System.out.println("Test score 1:  " + testScore1);
+        System.out.println("Test score 2:  " + testScore2);
+        System.out.println("Test score 3:  " + testScore3);
+        System.out.println("The average of 3 test scores is:  " + df.format(average));
+    }
+}
