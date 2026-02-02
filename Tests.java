b26014d8089ae4319e6158a58f267df553562556
JmorgaN-34 <shaddix.morgan@gmail.com>
2026-02-02 11:55:23 -0600

    Module 2 Assignment 2 Tests.java calculates tests average

--- Tests.java
+++ Tests.java
@@ -6,15 +6,15 @@ import java.text.DecimalFormat;
 
 public class Tests {
     public static void main(String[] args) {
-
+       //initialize the three test scores 
         double testScore1 = 88.2;
         double testScore2 = 78.9;
         double testScore3 = 97.6;
-
+      //calculates the average of all three test scores
         double average = (testScore1 + testScore2 + testScore3) / 3;
 
         DecimalFormat df = new DecimalFormat("0.00");
-
+       //prints results of individual test scores and the average of all three tests
         System.out.println("Test score 1:  " + testScore1);
         System.out.println("Test score 2:  " + testScore2);
         System.out.println("Test score 3:  " + testScore3);
